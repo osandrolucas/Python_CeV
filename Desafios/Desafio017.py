fb3e2fa6dee2e66e13c784afa99fbb9dcbd759d6
Sandro Lucas <sandro.ferreira@gruporbs.com.br>
2018-07-27 13:38:00 -0300

    Exercicio 17

--- Desafios/Desafio017.py
+++ Desafios/Desafio017.py
@@ -1,2 +1,14 @@
 #FUP que leia o comprimento do cateto oposto e do cateto adjacente de um triângulo retângulo.
 #Calcule e mostre o comprimento da hipotenusa
+print('Maneira com Conceito Matemático!')
+co = float(input('Comprimento do cateto oposto: '))
+ca = float(input('Comprimento do cateto adjacente: '))
+hi = (co ** 2 + ca ** 2) ** (1/2)
+print('A hipotenusa vai medir {:.2f}'.format(hi))
+
+from math import hypot
+print('\nManeira utilizando a Math')
+cos = float(input('Comprimento do cateto oposto: '))
+cat = float(input('Comprimento do cateto adjacente: '))
+hip = hypot(cos , cat)
+print('A hipotenusa vai medir {:.2f}'.format(hip))
