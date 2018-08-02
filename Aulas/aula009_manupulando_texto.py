3334a1b2e8423035218174fedffee311333fdb07
Sandro Lucas <sandro.ferreira@gruporbs.com.br>
2018-08-02 13:45:49 -0300

    Fase 09 - inicio

---
+++ Aulas/aula009_manupulando_texto.py
@@ -0,0 +1,11 @@
+print(
+    'Python – Fase 09 – Manipulando Strings'
+    '\nNessa aula, vamos aprender operações com String no Python. '
+    '\nAs principais operações que vamos aprender são o Fatiamento de String, '
+    'Análise com len(), count(), find(), \ntransformações com replace(), upper(), '
+    'lower(), capitalize(), title(), strip(), junção com join().'
+)
+
+#Para o python, toda cadeia de texto está entre aspas simples ou aspas duplas
+#Cadeia de caracteres = Strings
+
