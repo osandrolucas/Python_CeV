28f0bda818c0b0811b4b291fe3ee54844c16a5b8
Sandro Lucas <sandro.ferreira@gruporbs.com.br>
2018-08-02 12:33:24 -0300

    Exercicio 18 e 19

--- Desafios/Desafio019.py
+++ Desafios/Desafio019.py
@@ -1,2 +1,11 @@
 #Um professor quer sortear um dos seus quatro alunos para apagar o quadro.
 #Faça um programa que ajude ele. Lendo o nome deles e escrevendo o nome do escolhido.
+from random import choice
+print('Professor: Preciso de ajuda para apagar o quadro...')
+n1 = str(input('Digite o nome do primeiro aluno: '))
+n2 = str(input ('Informe o nome do segundo aluno: '))
+n3 = str(input('Informe o nome do terceito aluno: '))
+n4 = str(input('Informe o nome do quarto aluno: '))
+listaAlunos = [n1,n2,n3,n4]
+escolhido = choice(listaAlunos)
+print('O aluno escolhido foi: {}'.format(escolhido))
--- Desafios/Desafio020.py
+++ Desafios/Desafio020.py
@@ -1,2 +1,11 @@
 #O mesmo professor do desafio anterior, quer sortear a ordem da apresentação de trabalhos dos alunos.
 #FUP que leia o nome dos quatro alunos e mostre a ordem sorteada.
+import random as a #alias
+print('Informe os nomes dos alunos que apresentarão o trabalho: ')
+n1 = str(input('Digite o nome do primeiro aluno: '))
+n2 = str(input ('Informe o nome do segundo aluno: '))
+n3 = str(input('Informe o nome do terceito aluno: '))
+n4 = str(input('Informe o nome do quarto aluno: '))
+lista = [n1,n2,n3,n4]
+a.shuffle(lista) #embaralhando
+input('A ordem de apresentação será {}'.format(lista))
