6565c5e8e9bb303353899c6e36349660855f8f73
Sandro Lucas <osandrolucas@gmail.com>
2018-08-02 13:19:52 -0300

    21 no brainer mp3

--- Desafios/Desafio021.py
+++ Desafios/Desafio021.py
@@ -1,1 +1,20 @@
-#Faça um programa que abra e reproduza o audio de um arquivo mp3.
+#from pygame import *
+from playsound import playsound
+print('Desafio: Faça um programa que abra e reproduza o audio de um arquivo mp3.')
+
+#Primeira opção
+#mixer.init()
+#mixer.music.load('brainer.mp3')
+#mixer.music.play()
+#mixer.event.wait() #Espera a musica terminar (não funcionou)
+#input() #Tive que colocar isso para funcionar a pygame
+
+#Segunda opção
+playsound('brainer.mp3')
+
+#Terceira forma
+#mixer.init()
+#mixer.music.load('brainer.mp3')
+#mixer.music.play()
+#while mixer.music.get_busy():
+#    time.Clock().tick(10)
