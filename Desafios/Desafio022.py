e812b5dd585e0556d2b84a198e0a2fa8256bcab8
Sandro Lucas <30708527+osandrolucas@users.noreply.github.com>
2018-08-06 20:46:05 -0300

    Desafio 22 feito
    feito em https://repl.it/repls/UnconsciousUsedGenericsoftware

--- Desafios/Desafio022.py
+++ Desafios/Desafio022.py
@@ -1,5 +1,12 @@
 print("""Exercício Python 022: Crie um programa que leia o nome completo de uma pessoa e mostre:
 – O nome com todas as letras maiúsculas e minúsculas;
 – Quantas letras ao todo (sem considerar espaços);
-– Quantas letras tem o primeiro nome.""")
-nome = print('\nDigite seu nome completo: ')
+– Quantas letras tem o primeiro nome.\n""")
+
+nome = str(input('Nome: '))
+print('O nome em maiúsculo: {}'.format(nome.upper()))
+print('O nome em minúsculo: {}'.format(nome.lower()))
+print('Qual o tamanho do nome sem considerar espaços? {}'.format(len(nome.strip())))
+dividido = nome.split()
+print('Quantas letras tem o primeiro nome? {}'.format(len(dividido[0])))
+
