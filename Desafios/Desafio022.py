792db991d2e8b2bff0fc1270d6b7df830de639e0
Sandro Lucas <30708527+osandrolucas@users.noreply.github.com>
2018-08-06 20:52:41 -0300

    correção
    -count

--- Desafios/Desafio022.py
+++ Desafios/Desafio022.py
@@ -6,7 +6,7 @@ print("""Exercício Python 022: Crie um programa que leia o nome completo de uma
 nome = str(input('Nome: '))
 print('O nome em maiúsculo: {}'.format(nome.upper()))
 print('O nome em minúsculo: {}'.format(nome.lower()))
-print('Qual o tamanho do nome sem considerar espaços? {}'.format(len(nome.strip())))
+print('Qual o tamanho do nome sem considerar espaços? {}'.format(len(nome) -nome.count(' ')))
 dividido = nome.split()
 print('Quantas letras tem o primeiro nome? {}'.format(len(dividido[0])))
 
