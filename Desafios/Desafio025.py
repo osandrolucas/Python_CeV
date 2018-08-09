4bc7924c9888bb2c5317ac73afb819207aaed79e
Sandro Lucas <sandro.ferreira@gruporbs.com.br>
2018-08-09 13:07:50 -0300

    Exercicios de Manipulação de Textos

--- Desafios/Desafio025.py
+++ Desafios/Desafio025.py
@@ -1,3 +1,3 @@
 print("""Exercício Python 025: Crie um programa que leia o nome de uma pessoa e diga se ela tem SILVA" no nome.""")
-n = str(input('Insira seu nome: ')).strip()
+n = str(input('Insira seu nome:')).strip()
 print('Seu nome tem Silva? {}'.format('SILVA' in n.upper()))
